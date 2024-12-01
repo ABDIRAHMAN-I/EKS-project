b9005079f96f29341266c80cd44fabe7efb533a1
ABDIRAHMAN-I <abdirahman.i@hotmail.com>
2024-12-01 02:23:32 +0000

    modified variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,5 @@
 variable "ecr_repository_name" {
   description = "name of ecr repository"
   type        = string
-  default     = "tetras-app"
-}
-
-variable "s3_bucket_name" {
-  description = "name of s3 bucket"
-  type        = string
-
-
+  default     = "eks-tetras-project"
 }
