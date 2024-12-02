208a688c1fcef95b88cf5b71704cd3c5b18569a8
ABDIRAHMAN-I <abdirahman.i@hotmail.com>
2024-12-02 07:25:06 +0000

    create main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,39 @@ module "aws_ecr_repository" {
 }
 
 
+module "aws_vpc" {
+  source = "./modules/vpc"
+}
+
+
+module "aws_subnet" {
+  source = "./modules/vpc"  
+}
+
+
+module "aws_internet_gateway" {
+  source = "./modules/vpc"
+  
+}
+
+
+module "aws_route_table" {
+  source = "./modules/vpc"
+}
+
+module "aws_route_table_association" {
+  source = "./modules/vpc"
+}
+
+module "aws_eip" {
+  source = "./modules/vpc"
+}
+
+module "aws_nat_gateway" {
+  source = "./modules/vpc"
+}
+
+module "aws_security_group" {
+  source = "./modules/vpc"
+}
+
